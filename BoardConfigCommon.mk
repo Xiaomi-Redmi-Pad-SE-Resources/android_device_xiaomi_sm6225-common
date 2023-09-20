324656625b425157f12615d0ee3bc95c1566c888
Sugakesshaa <sugakesshaa@gmail.com>
2023-09-20 21:52:36 +0800

    sm6225-common: Inherit aosp device_framework_matrix
    - for PixelOS
    
    Change-Id: Iadfac307ef21bee5542f462f5a137798be77737b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -214,7 +214,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 ifneq ($(TARGET_IS_TABLET),true)
