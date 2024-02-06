350186b23d196c179a3d77fd8734d4d3b4f34ec9
Albert Tang <tangalbert919@yahoo.com>
2024-02-06 12:51:13 -0600

    sm6225-common: Don't store media config files locally
    Change-Id: Ieb27d0c570aa2888038b4ac0cc69559ff6c8c8e7

--- common.mk
+++ common.mk
@@ -288,9 +288,6 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx_plugin.vendor
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc)
-
-PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
