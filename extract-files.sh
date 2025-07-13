a6bd36fbe8083cd6ad92eb8d44df53de2a151f3f
johnmart19 <johnmart19@users.noreply.github.com>
2025-07-13 12:31:31 +0530

    sm6225-common: Add libaudioclient_shim for libwfdservice
    Android 16 compilation error: libwfdservice.so: error: Unresolved symbol:
     _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_t
     libwfdservice.so: note: libwfdservice.so: note: Some dependencies might be
     changed, thus the symbol(s) above cannot be resolved. libwfdservice.so: note:
     Please re-build the prebuilt file:
     out/soong/.intermediates/vendor/xiaomi/sm6225-common-common/libwfdservice/android_arm64_armv8-2a-dotprod_cortex-a76_shared/libwfdservice.so.
     libwfdservice.so: note: libwfdservice.so: note: If this is a new prebuilt
     file and it is designed to have unresolved symbols, add one of the following
     properties: libwfdservice.so: note:   Android.bp: allow_undefined_symbols:
     true, libwfdservice.so: note:   Android.mk: LOCAL_ALLOW_UNDEFINED_SYMBOLS :=
     true

--- extract-files.sh
+++ extract-files.sh
@@ -81,6 +81,7 @@ function blob_fixup() {
             ;;
         system_ext/lib64/libwfdservice.so)
             [ "$2" = "" ] && return 0
+            grep -q "libshim_sensorndkbridge.so" "${2}" || "${PATCHELF}" --add-needed "libaudioclient_shim.so" "${2}"
             "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V4-cpp.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.security.keymint-service-qti|vendor/lib64/libqtikeymint.so)
